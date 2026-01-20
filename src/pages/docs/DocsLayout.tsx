22efe6a5d5b49c5d575bcb68d796f9008dcf1a48
upsaurav12 <sauravup041103@gmail.com>
2026-01-20 13:15:11 +0530

    add: changes

--- src/pages/docs/DocsLayout.tsx
+++ src/pages/docs/DocsLayout.tsx
@@ -13,9 +13,19 @@ export function DocsLayout({ children }: DocsLayoutProps) {
       <Header />
       <div className="flex-1 flex w-full">
         <DocsSidebar />
-        <main className="flex-1 py-8 px-6 lg:px-12 max-w-4xl">
-          {children}
-        </main>
+<main
+  className="
+    flex-1
+    w-full
+    px-4 sm:px-6 lg:px-12
+    py-6 sm:py-8 lg:py-10
+    max-w-none lg:max-w-4xl
+    mx-auto
+  "
+>
+  {children}
+</main>
+
         <TableOfContents />
       </div>
       <Footer />
